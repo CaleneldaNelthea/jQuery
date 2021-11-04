c6c3b347ff305fa74bf29628cd6ccf0068f9a8c1
Arasjie <martijn.valkenborgh@student.pxl.be>
2021-11-04 18:25:33 +0100

    Jquery update

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// test if loaded after document
+// $(document).ready(function () {
+//   $("h1").css("color, "red");
+// });
+
+// loaded end of body
+// $("h1").css("color, "red");
+
+// set value
+// $("h1").css("color", "green");
+// get value
+// console.log($("h1").css("color"));
+
+//ad multiple classes
+$("h1").addClass("big-title margin-50");
+// removeClass, hasClass
+
+//text
+$("button").html("<em>Hey</em>");
+
+$("h1").text("bye");
